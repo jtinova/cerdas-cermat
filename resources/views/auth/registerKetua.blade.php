eb3bb7dcc1ade0b19321db96df03014843087938
Rizal Fahlevi <rizalfahlevi810@gmail.com>
2024-03-28 10:44:31 +0700

    redesign auth

--- resources/views/auth/registerKetua.blade.php
+++ resources/views/auth/registerKetua.blade.php
@@ -2,6 +2,110 @@
 
 @section('container')
     <div class="container">
+
+        <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+                        <div class="d-flex justify-content-center py-4">
+                            <a href="/" class="logo d-flex align-items-center w-auto">
+                                <img src="{{ asset('assets/img/logo/Logo.png') }}" alt="cerdas cermat">
+                                <span class="d-none d-lg-block">erdas cermat</span>
+                            </a>
+                        </div><!-- End Logo -->
+
+                        <div class="card mb-3">
+
+                            <div class="card-body container">
+
+                                <div class="pt-4 pb-2">
+                                    <h5 class="card-title text-center pb-0 fs-4">Buat Akun</h5>
+                                    <p class="text-center small">Masukkan data Anda untuk membuat Akun</p>
+                                </div>
+
+                                <div class="row text-center mb-5">
+                                    <div class="col">
+                                        <button class="btn btn-primary w-100">Ketua</button>
+                                    </div>
+                                    <div class="col">
+                                        <a href="/register/anggota">
+                                            <button class="btn w-100 mb-2 mb-md-0">Anggota</button>
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <form action="/register/ketua" method="POST">
+                                    @csrf
+                                    <div class="col-12 mb-3">
+                                        <label for="nama_tim" class="form-label">Nama Tim</label>
+                                        <input type="text" name="nama_tim" class="form-control" id="nama_tim" required>
+                                        <div class="invalid-feedback">Tolong, masukkan nama Tim</div>
+                                    </div>
+
+                                    <div class="col-12 mb-3">
+                                        <label for="institusi" class="form-label">Institusi</label>
+                                        <input type="text" name="institusi" class="form-control" id="institusi" required>
+                                        <div class="invalid-feedback">Tolong, masukkan nama Institusi</div>
+                                    </div>
+
+                                    <div class="col-12 mb-3">
+                                        <label for="nama_ketua" class="form-label">Nama Lengkap</label>
+                                        <input type="text" name="nama_ketua" class="form-control" id="nama_ketua"
+                                            required>
+                                        <div class="invalid-feedback">Tolong, masukkan nama</div>
+                                    </div>
+
+                                    <div class="col-12 mb-3">
+                                        <label for="email" class="form-label">Email Anda</label>
+                                        <input type="email" name="email" class="form-control" id="email" required>
+                                        <div class="invalid-feedback">Tolong masukkan nama yang sesuai!</div>
+                                        @error('email')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="col-12 mb-3">
+                                        <label for="password" class="form-label">Password</label>
+                                        <div class="input-group">
+                                            <input type="password" class="form-control" id="password" name="password"
+                                                required>
+                                            <button type="button" class="btn-show-password input-group-text"
+                                                onclick="togglePasswordVisibility()">
+                                                <i id="password-toggle-icon" class="bi bi-eye-slash"></i>
+                                            </button>
+                                        </div>
+                                        <div class="invalid-feedback">Tolong masukkan password Anda</div>
+                                    </div>
+
+                                    <div>
+                                        <input type="text" name="level" id="level" value="peserta" hidden>
+                                    </div>
+                                    <div>
+                                        <input type="text" name="status" id="status" value="ketua" hidden>
+                                    </div>
+
+                                    <div class="col-12 mb-3">
+                                        <button class="btn btn-primary w-100" type="submit">Create Account</button>
+                                    </div>
+
+                                    <div class="col-12">
+                                        <p class="small mb-0">Telah memiliki akun? <a href="/login">Log in</a>
+                                        </p>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+    </div>
+
+    {{-- <div class="container">
         <div class="row">
             <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
                 <div class="card border-0 shadow rounded-3 my-5">
@@ -71,7 +175,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
     <script>
         function togglePasswordVisibility() {
             var passwordInput = document.getElementById("password");
--- resources/views/auth/token.blade.php
+++ resources/views/auth/token.blade.php
@@ -2,6 +2,45 @@
 
 @section('container')
     <div class="container">
+
+        <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+                        <div class="d-flex justify-content-center py-4">
+                            <a href="/" class="logo d-flex align-items-center w-auto">
+                                <img src="{{ asset('assets/img/logo/Logo.png') }}" alt="cerdas cermat">
+                                <span class="d-none d-lg-block">erdas cermat</span>
+                            </a>
+                        </div><!-- End Logo -->
+
+                        <div class="card mb-3">
+
+                            <div class="card-body">
+
+                                <div class="pt-4 pb-2">
+                                    <h5 class="card-title text-center pb-0 fs-4 mb-4">Token Tim Anda</h5>
+                                    <p class="fw-bold mb-4 text-center">{{ $token }}</p>
+                                    <p class="text-center small mb-3">Mohon simpan Token ini, yang akan digunakan untuk pendaftaran anggota kelompok.</p>
+                                    <a href="/login">
+                                        <button class="btn btn-primary w-100" type="submit">
+                                            Masuk
+                                        </button>
+                                    </a>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+    </div>
+
+    {{-- <div class="container">
         <div class="row">
             <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
                 <div class="card border-0 shadow rounded-3 my-5">
@@ -19,5 +58,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 @endsection
